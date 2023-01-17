2cc16d502f59204b037f7db695a30da3d1928fbe
Pumpkin-3906 <anna88x88@gmail.com>
2023-01-17 00:26:38 -0600

    modify StreamTestLogger struct

--- e2etest.go
+++ e2etest.go
@@ -1,16 +1,14 @@
 package terraform_module_test_helper
 
 import (
-	"fmt"
+	"bytes"
 	"fmt"
 	"github.com/stretchr/testify/require"
+	"io"
 	"log"
 	"math/rand"
 	"os"
-	"strings"
-	"log"
-	"math/rand"
-	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 	"time"
@@ -19,7 +17,6 @@ import (
 	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
-	terratest "github.com/gruntwork-io/terratest/modules/testing"
 	terratest "github.com/gruntwork-io/terratest/modules/testing"
 )
 
@@ -49,15 +46,15 @@ func record() {
 	}
 }
 
-var _ logger.TestLogger = &FileTestLogger{}
+var _ logger.TestLogger = &StreamTestLogger{}
 
-type FileTestLogger struct {
-	// tempFilePath string
-	stream STREAM
-	ch chan
+type StreamTestLogger struct {
+	exampleName string
+	stream      io.ReadWriter
+	ch          chan string
 }
 
-func (f *FileTestLogger) Logf(t terratest.TestingT, format string, args ...interface{}) {
+func (f *StreamTestLogger) Logf(t terratest.TestingT, format string, args ...interface{}) {
 	tempFile, err := os.OpenFile(f.tempFilePath, os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -65,122 +62,41 @@ func (f *FileTestLogger) Logf(t terratest.TestingT, format string, args ...inter
 	logger.DoLog(t, 3, tempFile, fmt.Sprintf(format, args...))
 }
 
-func NewFileTestLogger() *FileTestLogger {
-
+func NewStreamTestLogger(exName string, rw io.ReadWriter) *StreamTestLogger {
+	return &StreamTestLogger{
+		exampleName: exName,
+		stream:      rw,
+	}
 }
 
-func (f *FileTestLogger) OpenFile() error {
+func (f *StreamTestLogger) OpenFile() error {
 	// file stream
-	exampleName := strings.Split(exampleRelativePath, "/")[1]
-	tempFileDir := fmt.Sprintf("%s/test%s%d", os.TempDir(), exampleName, rand.Intn(100))
-	tempFile, err := os.OpenFile(tempFileDir, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	tempFilePath := fmt.Sprintf("%s/test%s%d", os.TempDir(), f.exampleName, rand.Intn(100))
+	tempFile, err := os.OpenFile(tempFilePath, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	f.stream = xxx
+	f.stream = make([]byte)
 }
 
-func (f *FileTestLogger) OpenMemory() error {
+func (f *StreamTestLogger) OpenMemory() error {
 	// io buffer?
-	f.stream = xxx
+	f.stream = new(bytes.Buffer)
 }
 
-func (f *FileTestLogger) Close() {
-	f.stream.close()
-}
-
-func (f *FileTestLogger) Listen() {
-	// global make chan, go 监听 chan，打印到 stdout
-	// init() 里面创建一个
-	f.chan = xxx
-	io.copy to os.Stdout
-	release
-}
-
-
-func PrepareFile(exampleRelativePath string) string {
-	exampleName := strings.Split(exampleRelativePath, "/")[1]
-	tempFileDir := fmt.Sprintf("%s/test%s%d", os.TempDir(), exampleName, rand.Intn(100))
-	tempFile, err := os.OpenFile(tempFileDir, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer tempFile.Close()
-	return tempFileDir
-}
-
-var ch1 = make(chan string)
-var ch2 = make(chan string)
-
-func init() {
-	println("=> init")
-	go record()
-}
-
-func record() {
-	for {
-		// 收到一个地址，打开临时文件，读取，关掉文件
-		tempFileDir := <-ch1
-		fmt.Printf("======== FileDir: [%s] ========\n", tempFileDir)
-
-		//contents, err := os.ReadFile(tempFileDir)
-		//if err != nil {
-		//	log.Panicf("Failed reading file: %s", err)
-		//}
-		//fmt.Printf("\n%s", string(contents))
-
-		ch2 <- "ok"
+func (f *StreamTestLogger) Close() {
+	closer, ok := f.stream.(io.Closer)
+	if ok == true {
+		f.stream.Close()
 	}
 }
 
-var _ logger.TestLogger = &FileTestLogger{}
-
-type FileTestLogger struct {
-	// tempFilePath string
-	stream STREAM
-	ch chan
+func (f *StreamTestLogger) Listen() {
+	// init() 里面 global make chan, 新建 goroutine 监听 chan，打印到 stdout，release
+	f.ch = make(chan string)
+	io.Copy(os.Stdout, f.stream.(io.Reader))
+	// release
 }
-
-func (f *FileTestLogger) Logf(t terratest.TestingT, format string, args ...interface{}) {
-	tempFile, err := os.OpenFile(f.tempFilePath, os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	logger.DoLog(t, 3, tempFile, fmt.Sprintf(format, args...))
-}
-
-func NewFileTestLogger() *FileTestLogger {
-
-}
-
-func (f *FileTestLogger) OpenFile() error {
-	// file stream
-	exampleName := strings.Split(exampleRelativePath, "/")[1]
-	tempFileDir := fmt.Sprintf("%s/test%s%d", os.TempDir(), exampleName, rand.Intn(100))
-	tempFile, err := os.OpenFile(tempFileDir, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	f.stream = xxx
-}
-
-func (f *FileTestLogger) OpenMemory() error {
-	// io buffer?
-	f.stream = xxx
-}
-
-func (f *FileTestLogger) Close() {
-	f.stream.close()
-}
-
-func (f *FileTestLogger) Listen() {
-	// global make chan, go 监听 chan，打印到 stdout
-	// init() 里面创建一个
-	f.chan = xxx
-	io.copy to os.Stdout
-	release
-}
-
 
 func PrepareFile(exampleRelativePath string) string {
 	exampleName := strings.Split(exampleRelativePath, "/")[1]
@@ -195,7 +111,6 @@ func PrepareFile(exampleRelativePath string) string {
 
 func RunE2ETest(t *testing.T, moduleRootPath, exampleRelativePath string, option terraform.Options, assertion func(*testing.T, TerraformOutput)) {
 	t.Parallel()
-	t.Parallel()
 	option = retryableOptions(t, option)
 	tmpDir := test_structure.CopyTerraformFolderToTemp(t, moduleRootPath, exampleRelativePath)
 	if err := rewriteHcl(tmpDir, ""); err != nil {
@@ -204,26 +119,19 @@ func RunE2ETest(t *testing.T, moduleRootPath, exampleRelativePath string, option
 	option.TerraformDir = tmpDir
 
 	// create or open a temp log file
-	tempFileDir := PrepareFile(exampleRelativePath)
+	//tempFilePath := PrepareFile(exampleRelativePath)
+	exampleName := strings.Split(exampleRelativePath, "/")[1]
 
 	//option.Logger = logger.Terratest
-	option.Logger = logger.New(FileTestLogger{tempFileDir})
+	option.Logger = logger.New(&StreamTestLogger{
+		exampleName: exampleName,
+	})
 	// defer file.close()
 
 	// option.NoColor = true
 
-	defer destroy1(t, tempFileDir, option)
+	defer destroy1(t, tempFilePath, option)
 
-	// create or open a temp log file
-	tempFileDir := PrepareFile(exampleRelativePath)
-
-	//option.Logger = logger.Terratest
-	option.Logger = logger.New(FileTestLogger{tempFileDir})
-	// defer file.close()
-
-	// option.NoColor = true
-
-	defer destroy1(t, tempFileDir, option)
 	terraform.InitAndApply(t, &option)
 	if err := initAndPlanAndIdempotentAtEasyMode(t, option); err != nil {
 		t.Fatalf(err.Error())
@@ -235,8 +143,8 @@ func RunE2ETest(t *testing.T, moduleRootPath, exampleRelativePath string, option
 	// t.Failed()
 }
 
-func destroy1(t *testing.T, tempFileDir string, option terraform.Options) {
-	defer printLog(tempFileDir)
+func destroy1(t *testing.T, tempFilePath string, option terraform.Options) {
+	defer printLog(tempFilePath)
 
 	option.MaxRetries = 10
 	option.TimeBetweenRetries = time.Minute
@@ -254,25 +162,6 @@ func printLog(tempFileDir string) {
 	_ = <-ch2
 
 	// t.Failed()
-}
-
-func destroy1(t *testing.T, tempFileDir string, option terraform.Options) {
-	defer printLog(tempFileDir)
-
-	option.MaxRetries = 10
-	option.TimeBetweenRetries = time.Minute
-	option.RetryableTerraformErrors = map[string]string{
-		".*": "Retry destroy on any error",
-	}
-	_, err := terraform.RunTerraformCommandE(t, &option, terraform.FormatArgs(&option, "destroy", "-auto-approve", "-input=false", "-refresh=false")...)
-	require.NoError(t, err)
-}
-
-func printLog(tempFileDir string) {
-	// 把原来创建的临时文件关掉
-	// 把临时文件的地址发给 goroutine record()
-	ch1 <- tempFileDir
-	_ = <-ch2
 }
 
 func destroy(t *testing.T, option terraform.Options) {
